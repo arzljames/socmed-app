c925e45c58bb13b8c92aa1650691e1bbfc902f47
Arzl James <70579069+arzljames@users.noreply.github.com>
2023-02-12 03:38:21 +0800

    Update WelcomeModal.tsx

--- components/Home/WelcomeModal.tsx
+++ components/Home/WelcomeModal.tsx
@@ -4,7 +4,7 @@ import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import { PUT } from "../../utils/api/api";
 import useRefreshData from "../../hooks/useRefreshData";
 import useUserData from "../../hooks/useUserData";
-import { UserLoggedInProps } from "../../interface";
+import { UserProps } from "../../interface";
 
 const WelcomeModal = ({
   setIsNewUser,
@@ -12,7 +12,7 @@ const WelcomeModal = ({
   setIsNewUser: Dispatch<SetStateAction<Boolean>>;
 }): ReactElement => {
   const router = useRouter();
-  const { user } = useUserData() as { user: UserLoggedInProps };
+  const { user } = useUserData() as { user: UserProps };
   const handleUpdateUser = async () => {
     try {
       const res = await PUT("/api/user/new-user/", user.access_token);
